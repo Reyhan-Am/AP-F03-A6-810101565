08d9ce2b346f2b697e0fae16ed6aa1a4cc9a8deb
Reyhan-Am <123007836+Reyhan-Am@users.noreply.github.com>
2024-12-27 18:47:54 +0330

    Update Utaste.hpp

--- Utaste.hpp
+++ Utaste.hpp
@@ -5,11 +5,21 @@
 #include "post.hpp"
 #include "delete.hpp"
 #include "get.hpp"
+#include "exception.hpp"
 
 class UTaste
 {
 public:
     UTaste(string districts_path, string restaurants_path);
+    void checkCommand();
+    void setLogin(string username);
+    void setLogout(string username);
+    void setUserDistrict(string *districti_ptr, string username);
+    void setReserve(string *&restaurant_name_ptr, string *&table_id_ptr, string *&start_time_ptr,
+                    string *&end_time_ptr,
+                    string *&foods_ptr);
+    void setNewUser(pair<USERNAME, USER_DATA> &new_user);
+    void deleteReserve(vector<string> &command_wordl);
     void printing();
 
 protected:
