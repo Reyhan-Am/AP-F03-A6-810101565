aef49aecf162d121af6bc65579f8b30c7837561a
Reyhan-Am <123007836+Reyhan-Am@users.noreply.github.com>
2024-12-27 18:34:52 +0330

    Add new constants to constants.cpp

---
+++ constants.cpp
@@ -0,0 +1,17 @@
+#include "constants.hpp"
+
+const string BAD_REQ = "Bad Request";
+const string OK = "OK";
+const string EMPTY = "Empty";
+const string NOT_FOUND = "Not Found";
+const string PERMISSION = "Permission Denied";
+const string PUT_MY_DISTRICT = "my_district";
+const string SIGNUP = "signup";
+const string LOGIN = "login";
+const string LOGOUT = "logout";
+const string RESERVE = "reserve";
+const string RESERVES = "reserves";
+const string GET_RESTAURANTS = "restaurants";
+const int CONST_WORD_NO = 3;
+const string GET_DISTRICTS = "districts";
+const string GET_RESTAURANT = "restaurant_detail";
---
+++ constants.hpp
@@ -0,0 +1,23 @@
+#ifndef CONSTANTS_HPP
+#define CONSTANTS_HPP
+
+#include <string>
+using std::string;
+
+extern const string BAD_REQ;
+extern const string OK;
+extern const string EMPTY;
+extern const string NOT_FOUND;
+extern const string PERMISSION;
+extern const string PUT_MY_DISTRICT;
+extern const string SIGNUP;
+extern const string LOGIN;
+extern const string LOGOUT;
+extern const string RESERVE;
+extern const string RESERVES;
+extern const string GET_RESTAURANTS;
+extern const int CONST_WORD_NO;
+extern const string GET_DISTRICTS;
+extern const string GET_RESTAURANT;
+
+#endif
