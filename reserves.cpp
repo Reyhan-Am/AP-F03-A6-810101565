d90dabe280c4c8deb34cea2e5d0deb0dfa2463a2
Reyhan-Am <123007836+Reyhan-Am@users.noreply.github.com>
2025-01-07 23:16:47 +0330

    update reserve class

--- reserves.cpp
+++ reserves.cpp
@@ -1,44 +1,41 @@
 #include "reserves.hpp"
-Reserve::Reserve(string owner_, vector<string> food_, pair<int, int> times_, int id_, int table_num_)
+Reserve::Reserve(string owner_, vector<string> food_, pair<int, int> times_, int id_, int table_num_, int expense_, int pure_expense_)
 {
     owner = owner_;
     food = food_;
     times = times_;
     table_num = table_num_;
+    expense = expense_;
     id = id_;
+    pure_expense = pure_expense_;
 }
-void Reserve::printReserveMessage(string &restaurant_name, int price)
+pair<int, int> Reserve::printReserveMessage(string &restaurant_name, int price, int &first_diss, int &food_diss, int &total_diss, int &final_diss)
 {
     cout << "Reserve ID: " << id << endl;
     cout << "Table " << table_num << " for " << times.first << " to " << times.second << " in " << restaurant_name << endl;
-    cout << "Price: " << price << endl;
+    cout << "Original Price: " << price << endl;
+    cout << "Order Amount Discount: " << total_diss << endl;
+    cout << "Total Item Specific Discount: " << food_diss << endl;
+    cout << "First Order Discount: " << first_diss << endl;
+    cout << "Total Discount: " << final_diss << endl;
+    cout << "Total Price: " << price - final_diss << endl;
+    return {price - final_diss, price};
 }
-void Reserve::printUserReservesRI(string &restaurant_name)
+void Reserve::printUserReservesRI(string &restaurant_name, int &with_dis, int &without_dis)
 {
+    with_dis += expense;
+    without_dis += pure_expense;
     cout << id << ": " << restaurant_name << ' ' << table_num << ' ' << times.first << '-' << times.second;
-    if (food.size() == 0)
+    cout << ' ';
+    map<string, int> food_info;
+    for (auto f : food)
     {
-        cout << endl;
+        food_info[f]++;
     }
-    else
+    for (auto it = food_info.begin(); it != food_info.end(); it++)
     {
-        cout << ' ';
-        map<string, int> food_info;
-        for (auto f : food)
-        {
-            food_info[f]++;
-        }
-        for (auto it = food_info.begin(); it != food_info.end(); it++)
-        {
-            cout << (*it).first << "(" << (*it).second << ")";
-            if (next(it) != food_info.end())
-            {
-                cout << ' ';
-            }
-            else
-            {
-                cout << endl;
-            }
-        }
+        cout << (*it).first << "(" << (*it).second << ")" << ' ';
     }
+
+    cout << without_dis << ' ' << with_dis << endl;
 }
--- reserves.hpp
+++ reserves.hpp
@@ -6,13 +6,20 @@
 class Reserve
 {
 public:
-    Reserve(string owner_, vector<string> food_, pair<int, int> times_, int id_, int table_num_);
+    Reserve(string owner_, vector<string> food_, pair<int, int> times_, int id_, int table_num_, int expense_ = 0, int pure_expense_ = 0);
     pair<int, int> getTimes() { return times; };
     string getOwner() { return owner; };
+    int getExpense() { return expense; }
     string getTableNum() { return to_string(table_num); };
     vector<string> getFoods() { return food; };
-    void printReserveMessage(string &restaurant_name, int price);
-    void printUserReservesRI(string &restaurant_name);
+    void setExpense(int new_expense) { expense = new_expense; }
+    void setExpenses(pair<int, int> new_expenses)
+    {
+        expense = new_expenses.first;
+        pure_expense = new_expenses.second;
+    }
+    pair<int, int> printReserveMessage(string &restaurant_name, int price, int &first_diss, int &food_diss, int &total_diss, int &final_diss);
+    void printUserReservesRI(string &restaurant_name, int &with_dis, int &without_dis);
     int getID() { return id; };
 
 private:
@@ -20,6 +27,8 @@ private:
     vector<string> food;
     pair<int, int> times;
     int id;
+    int expense;
+    int pure_expense;
     int table_num;
 };
 
